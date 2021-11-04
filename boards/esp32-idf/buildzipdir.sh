c77a8688fe0ef4a080ca7fde7041ea45f31d2222
Furball The Great <furballthegreat@airmeter.io>
2021-11-04 02:37:32 +0000

    Fix?

--- boards/esp32-idf/buildzipdir.sh
+++ boards/esp32-idf/buildzipdir.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-mkdir build/esp32zip
-cp ../../opt/esp/idf/components/esptool_py/esptool/esptool.py build/esp32zip
-cp build/bootloader/bootloader.bin build/esp32zip
-cp build/partition_table/partition-table.bin build/esp32zip
-cp build/ota_data_initial.bin build/esp32zip
-cp build/main.bin build/esp32zip
-cp build/web.bin build/esp32zip
-cp boards/esp32-idf/flash* boards/esp32-idf/readme* build/esp32zip
-cp LICENSE boards/esp32zip
-cp webui/dist/bundle.js.LICENSE.txt boards/esp32zip
-zip build/esp32build.zip boards/esp32zip/*
+mkdir /tmp/build
+mkdir /tmp/build/esp32zip
+cp ../../opt/esp/idf/components/esptool_py/esptool/esptool.py /tmp/build/esp32zip
+cp build/bootloader/bootloader.bin /tmp/build/esp32zip
+cp build/partition_table/partition-table.bin /tmp/build/esp32zip
+cp build/ota_data_initial.bin /tmp/build/esp32zip
+cp build/main.bin /tmp/build/esp32zip
+cp build/web.bin /tmp/build/esp32zip
+cp boards/esp32-idf/flash* boards/esp32-idf/readme* /tmp/build/esp32zip
+cp LICENSE /tmp/build/esp32zip
+cp webui/dist/bundle.js.LICENSE.txt /tmp/build/esp32zip
+zip /tmp/build/esp32build.zip /tmp/build/esp32zip*
 
 

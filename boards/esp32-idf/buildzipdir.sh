743ae905da8524c8c9478270305775d08288ee4e
Furball The Great <furballthegreat@airmeter.io>
2021-11-04 02:16:23 +0000

    Workflow...

---
+++ boards/esp32-idf/buildzipdir.sh
@@ -0,0 +1,15 @@
+#!/bin.sh
+
+mkdir build/esp32zip
+cp ../../opt/esp/idf/components/esptool_py/esptool/esptool.py build/esp32zip
+cp build/bootloader/bootloader.bin build/esp32zip
+cp build/partition_table/partition-table.bin build/esp32zip
+cp build/ota_data_initial.bin build/esp32zip
+cp build/main.bin build/esp32zip
+cp build/web.bin build/esp32zip
+cp boards/esp32-idf/flash* boards/esp32-idf/readme* build/esp32zip
+cp LICENSE boards/esp32zip
+cp webui/dist/bundle.js.LICENSE.txt boards/esp32zip
+zip build/esp32build.zip boards/esp32zip/*
+
+
---
+++ boards/esp32-idf/readme.txt
@@ -0,0 +1,16 @@
+AirMeter.io ESP32 Build
+-----------------------
+
+This zip file contains a binary build of AirMeter.io for the ESP32 platform. 
+
+Linux
+-----
+
+sh flash.sh /dev/ttyUSB1
+
+
+Windows
+-------
+flash.bat COM3
+
+

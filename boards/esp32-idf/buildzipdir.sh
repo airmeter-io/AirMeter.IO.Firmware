eccdcc534c6042b453d99f2e923f423fd822ee17
Furball The Great <furballthegreat@airmeter.io>
2021-11-04 03:20:41 +0000

    Fix?

--- boards/esp32-idf/buildzipdir.sh
+++ boards/esp32-idf/buildzipdir.sh
@@ -11,6 +11,7 @@ cp build/web.bin /tmp/build/esp32zip
 cp boards/esp32-idf/flash* boards/esp32-idf/readme* /tmp/build/esp32zip
 cp LICENSE /tmp/build/esp32zip
 cp webui/dist/bundle.js.LICENSE.txt /tmp/build/esp32zip
-zip /tmp/build/esp32build.zip /tmp/build/esp32zip*
+ls /tmp/build/esp32zip
+zip /tmp/build/esp32build.zip /tmp/build/esp32zip/*
 
 

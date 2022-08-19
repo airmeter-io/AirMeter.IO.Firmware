1d16af3c950d27980b16790ea0fda4f483abf166
Furball The Great <furballthegreat@airmeter.io>
2022-08-19 19:55:26 +0100

    Disable battery voltage checks on ESP8266

--- components/BATTERY/include/BatteryManager.h
+++ components/BATTERY/include/BatteryManager.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "Common.h"
 
-
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 0, 0)    
 extern "C" {
     #include "esp_adc_cal.h"
 }
+#endif
+
 
 class BatteryManager {
     esp_adc_cal_characteristics_t _adcCharacteristics;
--- components/BATTERY/src/BatteryManager.cpp
+++ components/BATTERY/src/BatteryManager.cpp
@@ -3,6 +3,7 @@
 #define ADC_EN 14
 
 BatteryManager::BatteryManager() {
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 0, 0)    
     gpio_set_direction((gpio_num_t)ADC_EN, GPIO_MODE_OUTPUT);
     for(auto i = 0; i < 8; i++)
         adc1_config_channel_atten((adc1_channel_t)i, ADC_ATTEN_DB_11);
@@ -12,7 +13,7 @@ BatteryManager::BatteryManager() {
         (adc_bits_width_t)ADC_WIDTH_BIT_12, 
         1100, 
         &_adcCharacteristics);
-   
+#endif
 }
 
 BatteryManager::~BatteryManager() {
@@ -22,6 +23,7 @@ BatteryManager::~BatteryManager() {
 #define TAG "bat"
 
 float BatteryManager::GetBatteryVoltage() {
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 0, 0)    
     uint32_t reading;
   auto err = esp_adc_cal_get_voltage(
         (adc_channel_t)ADC1_CHANNEL_7,
@@ -38,4 +40,7 @@ float BatteryManager::GetBatteryVoltage() {
      gpio_set_level((gpio_num_t)ADC_EN, 0);
     if(err!=ESP_OK) printf("Error reading battery level: %d\n", err);
     return (((float)reading)/4096)*7.26;
+#else
+    return 0;
+#endif
 }

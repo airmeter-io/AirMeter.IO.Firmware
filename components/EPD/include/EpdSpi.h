4442a18baffead5eea09852f142f491d43d7ad46
Furball The Great <furballthegreat@airmeter.io>
2022-08-19 18:13:47 +0100

    Change Cappitalisation

---
+++ components/EPD/include/EpdSpi.h
@@ -0,0 +1,25 @@
+/* Implement IoInterface for SPI communication */
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
+
+
+#ifndef epdspi_h
+#define epdspi_h
+class EpdSpi 
+{
+  public:
+    spi_device_handle_t spi;
+
+    void cmd(const uint8_t cmd) ;
+    void data(uint8_t data) ;
+    void dataBuffer(uint8_t data);
+    void data(const uint8_t *data, int len) ;
+    
+    void reset(uint8_t millis) ;
+    void init(uint8_t frequency, bool debug) ;
+  private:
+    bool debug_enabled = true;
+};
+#endif
+// Note: using override compiler will issue an error for "changing the type"
+//       in case the type is changed.
